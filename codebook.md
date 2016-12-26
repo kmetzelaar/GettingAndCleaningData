47ba8ccc6c2341c9236781f27f662e4477693435
kmetzelaar <kmetzelaar@hotmail.com>
2016-12-26 05:23:15 -0500

    Changes to be committed: 	new file:   codebook.md 	created codebook
    	modified:   run_analysis.R
    	added codebook generating code to script

---
+++ codebook.md
@@ -0,0 +1,66 @@
+# Code Book
+#
+##Download/Unzip/Gather the data
+* URL for Data: `https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip`
+* File name stored in working directory: `Dataset.zip`
+* Name of the directory zip file unzips to: `UCI HAR Dataset`
+* Training experiment measurements read from: `./UCI HAR Dataset/test/X_train.txt`
+* Test experiment measurements read from: `./UCI HAR Dataset/test/X_test.txt`
+* Training subject numbers (volunteer numbers) read from: `./UCI HAR Dataset/test/subject_train.txt`
+* Test experiment measurements read from: `./UCI HAR Dataset/test/subject_test.txt`
+* Feature names (measurement names) read from: `./UCI HAR Dataset/features.txt`
+* Activity types read from: `./UCI HAR Dataset/activity_labels.txt`
+##Steps
+* Bind together the Training and Test subjects into one data frame `bindedSubject`
+* Bind together the Training and Test activities into one data frame `bindedActivity`
+* Combine the subject and activity data frames into one data frame `bindedSubjectActivity`
+* Bind the Training and Test experiment measurements into one data frame `bindedTrainTest`
+* Bind the subject, activity, and experiment measurements into one data frame `bindedSubjectActivityAndData`
+* Merge `activity_name` with `bindedSubjectActivityAndData` using  `activity_number` from each data frame as the key.
+* Merged data frame is call `mergedData`.
+* Subset `mergedData` to contain only the `std` and `mean` feature columns.
+* Use `aggregate` command, which computes summary statistics of data subsets, to calculate the mean along subject + activity
+## Overall Data Set
+### Key columns
+Column name         | Description
+--------------------|------------
+`subject_number`    | Subject/Volunteer Number (1-30
+`activity_number`   | Activity type (1-6)
+`activity_name`     | Label for activity:
+                    | WALKING (activity_number 1)
+                    | WALKING_UPSTAIRS (2)
+                    | WALKING_DOWNSTAIRS (3)
+                    | SITTING (4)
+                    | STANDING (5)
+                    | LAYING (6)
+
+### Feature Name Decoder
+Column name         | Description
+--------------------|------------
+`time`              | Time Domain Measurement
+`frequency`         | Frequency Domain Measurement
+`accelerometer`     | Measurement from Accelerometer
+`gyroscope`         | Measurement from Gyroscope
+`magnitude`         | Measurement of Magnitude
+`body`              | Measurement from body
+`gravity`           | Measurement of gravity
+`standard_deviation`| Standard deviation measurement
+`mean`              | Mean measurement
+`X`,`Y`, or `Z      | Axis of measurement
+## Tidy Data Set
+### Key columns
+Column name         | Description
+--------------------|------------
+`subject_number`    | Subject/Volunteer Number (1-30
+`activity_name`     | Label for activity, same as above for Overall Data Set
+
+### Feature Name Decoder
+* Same as above, except the data recorded are the means along `subject_number` + `activity_name`
+* The range of the means is [-1,1]
+
+## Data output files
+Name                | Description
+--------------------|------------
+`DataSet.txt`       | Overall data set with subject, activity, and all experimental measurements
+`TidyData.txt`      | Tidy data set with average of each variable for each activity and each subject
+
--- run_analysis.R
+++ run_analysis.R
@@ -2,43 +2,66 @@
 ## Course Project
 
 
-
-###################################
-## Download/Unzip/Gather the data##
-###################################
-
-
 ######Replace "filePath" with your working directory/folder############
 filePath <- "/Users/kmetzelaar/GettingAndCleaningData/Project" 
 
+## Create a function to write a new line to the codebook.md
+
+codebook <- function(...){
+        cat(..., "\n",file=codebookFile,append=TRUE, sep="")
+}
+codebookFile <- file.path(filePath,"codebook.md")
+file.remove(codebookFile)
+codebook("# Code Book")
+codebook("#")
+
+
+#############################################
+codebook("##Download/Unzip/Gather the data")##
+#############################################
+
 ## Change the following values as necessary based on updates to the data set and/or zip file
 fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+codebook("* URL for Data: `",fileURL,"`")
 fileName <- "Dataset.zip"
+codebook("* File name stored in working directory: `",fileName,"`")
 unzipDir <- "UCI HAR Dataset"
+codebook("* Name of the directory zip file unzips to: `",unzipDir,"`")
 trainDir <- "train"
 testDir <- "test"
 
-## Download data set to working directory
+## Download data set to working directory       
 download.file(fileURL,file.path(filePath,fileName))
 
 ## Unzip to local disk, "UCI HAR Dataset" sub-directory
 unzip(file.path(filePath,fileName), overwrite = TRUE, exdir = filePath)
 
 ## Read the Training and Test experiment measurements
+codebook("* Training experiment measurements read from: `",file.path(".",unzipDir,testDir,"X_train.txt"),"`")
 xTrainData <- read.table(file.path(filePath,unzipDir,trainDir,"X_train.txt"))
+codebook("* Test experiment measurements read from: `",file.path(".",unzipDir,testDir,"X_test.txt"),"`")
 xTestData <- read.table(file.path(filePath,unzipDir,testDir,"X_test.txt"))
 yTrainData <- read.table(file.path(filePath,unzipDir,trainDir,"y_train.txt"))
 yTestData <- read.table(file.path(filePath,unzipDir,testDir,"y_test.txt"))
 
 ## Read the volunteer (subject) list
+codebook("* Training subject numbers (volunteer numbers) read from: `",file.path(".",unzipDir,testDir,"subject_train.txt"),"`")
 trainSubject <- read.table(file.path(filePath,unzipDir,trainDir,"subject_train.txt"))
+codebook("* Test experiment measurements read from: `",file.path(".",unzipDir,testDir,"subject_test.txt"),"`")
 testSubject <- read.table(file.path(filePath,unzipDir,testDir,"subject_test.txt"))
 
 # Read the names of the "features," which are the names/categories of the experimental data collected
 features <- read.table(file.path(filePath,unzipDir,"features.txt"))
 # Normalize feature names to lower case
 features$V2 <- tolower(features$V2)
+codebook("* Feature names (measurement names) read from: `",file.path(".",unzipDir,"features.txt"),"`")
 
+# Read the names of the activities (WALKING, SITTING, etc.)
+activityLabels <- read.table(file.path(filePath,unzipDir,"activity_labels.txt"))
+codebook("* Activity types read from: `",file.path(".",unzipDir,"activity_labels.txt"),"`")
+
+## Assign names to columns
+names(activityLabels) <- c("activity_number", "activity_name")
 
 
 
@@ -47,28 +70,29 @@ features$V2 <- tolower(features$V2)
 ## Merge the training and the test sets to create one data set.##
 #################################################################
 
-# Read the names of the activities (WALKING, SITTING, etc.)
-activityLabels <- read.table(file.path(filePath,unzipDir,"activity_labels.txt"))
-## Assign names to columns
-names(activityLabels) <- c("activity_number", "activity_name")
+codebook("##Steps")
 
-## Bind together the Training and Test subjects into one data frame
+codebook("* Bind together the Training and Test subjects into one data frame `bindedSubject`")
 bindedSubject <- rbind(trainSubject, testSubject)
 
-## Bind together the Training and Test activities into one data frame
+
+codebook("* Bind together the Training and Test activities into one data frame `bindedActivity`")
 bindedActivity <- rbind(yTrainData, yTestData)
 
-## combine the subject and activity data frames
+
+codebook("* Combine the subject and activity data frames into one data frame `bindedSubjectActivity`")
 bindedSubjectActivity <- cbind(bindedSubject, bindedActivity)
-## assign names to columns
+## Assign names to columns
 names(bindedSubjectActivity) <- c("subject_number", "activity_number")
 
-## Bind together the Training and Test experiment measurements
+
+codebook("* Bind the Training and Test experiment measurements into one data frame `bindedTrainTest`")
 bindedTrainTest <- rbind(xTrainData, xTestData)
 ## Assign names to columns
 names(bindedTrainTest) <- features$V2
 
-## Combine the subject, activity, and experiment measurements into one data frame
+
+codebook("* Bind the subject, activity, and experiment measurements into one data frame `bindedSubjectActivityAndData`")
 bindedSubjectActivityAndData <- cbind(bindedSubjectActivity, bindedTrainTest)
 
 
@@ -80,9 +104,11 @@ bindedSubjectActivityAndData <- cbind(bindedSubjectActivity, bindedTrainTest)
 
 ## Merge the subject, activity, and experiment measurement data frame with the activityLabels
 ## This will add a column with the descriptive activity name (WALKING, SITTING, etc) to a new data frame
+codebook("* Merge `activity_name` with `bindedSubjectActivityAndData` using  `activity_number` from each data frame as the key.")
+codebook("* Merged data frame is call `mergedData`.")
 mergedData <- merge(bindedSubjectActivityAndData, activityLabels, by.x = "activity_number", all.x=TRUE)
 ## Re-order the columns such that activity name comes after subject number. Remove activity number.
-mergedData <- mergedData[ , c(2, 564, 3:563)]
+mergedData <- mergedData[ , c(2, 1, 564, 3:563)]
 #############################################################################
 ## At this point the data table "mergedData" achieves Project Goals #1 & #3##
 #############################################################################
@@ -96,11 +122,13 @@ mergedData <- mergedData[ , c(2, 564, 3:563)]
 
 ## Extract only the columns that contain mean and standard deviation measurements
 ## Don't forget to keep the columns that contain activity and subject information
+codebook("* Subset `mergedData` to contain only the `std` and `mean` feature columns.")
 mergedData <- mergedData[ , grepl("activity|subject|(mean|std)\\(\\)",names(mergedData))]
 #######################################################################
 ## At this point the data table "mergedData" achieves Project Goal #2##
 #######################################################################
 
+codebook("* Use `aggregate` command, which computes summary statistics of data subsets, to calculate the mean along subject + activity")
 
 
 
@@ -128,6 +156,37 @@ names(mergedData) <- tempNames
 #######################################################################
 
 
+codebook("## Overall Data Set")
+codebook("### Key columns")
+codebook("Column name         | Description")
+codebook("--------------------|------------")
+codebook("`subject_number`    | Subject/Volunteer Number (1-30")
+codebook("`activity_number`   | Activity type (1-6)")
+codebook("`activity_name`     | Label for activity:")
+codebook("                    | WALKING (activity_number 1)")
+codebook("                    | WALKING_UPSTAIRS (2)")
+codebook("                    | WALKING_DOWNSTAIRS (3)")
+codebook("                    | SITTING (4)")
+codebook("                    | STANDING (5)")
+codebook("                    | LAYING (6)")
+codebook("")
+
+codebook("### Feature Name Decoder")
+codebook("Column name         | Description")
+codebook("--------------------|------------")
+codebook("`time`              | Time Domain Measurement")
+codebook("`frequency`         | Frequency Domain Measurement")
+codebook("`accelerometer`     | Measurement from Accelerometer")
+codebook("`gyroscope`         | Measurement from Gyroscope")
+codebook("`magnitude`         | Measurement of Magnitude")
+codebook("`body`              | Measurement from body")
+codebook("`gravity`           | Measurement of gravity")
+codebook("`standard_deviation`| Standard deviation measurement")
+codebook("`mean`              | Mean measurement")
+codebook("`X`,`Y`, or `Z      | Axis of measurement")
+
+
+  
 
 
 ############################################################################
@@ -138,7 +197,24 @@ names(mergedData) <- tempNames
 
 ## Create Tidy Data
 ## Use "aggregate" command, which computes summary statistics of data subsets, to calculate the mean along subject + activity
-tidyData <- aggregate(mergedData[ , 3:68], by = list(activity = mergedData$activity_name, subject = mergedData$subject_number), FUN = mean)
+tidyData <- aggregate(mergedData[ , 4:69], by = list(activity_name = mergedData$activity_name, subject_number = mergedData$subject_number), FUN = mean)
+
+
+
+codebook("## Tidy Data Set")
+codebook("### Key columns")
+codebook("Column name         | Description")
+codebook("--------------------|------------")
+codebook("`subject_number`    | Subject/Volunteer Number (1-30")
+codebook("`activity_name`     | Label for activity, same as above for Overall Data Set")
+codebook("")
+
+codebook("### Feature Name Decoder")
+codebook("* Same as above, except the data recorded are the means along `subject_number` + `activity_name`")
+codebook("* The range of the means is [-1,1]")
+codebook("")
+
+
 
 ## Write overall data set with subject, activity, and experimental measurements to file
 write.table(mergedData, file.path(filePath,"DataSet.txt"), row.names = FALSE)
@@ -146,7 +222,16 @@ write.table(mergedData, file.path(filePath,"DataSet.txt"), row.names = FALSE)
 ## Write tidy data set with average of each variable for each activity and each subject.
 write.table(tidyData, file.path(filePath, "TidyData.txt"), row.names = FALSE)
 
-###############################
+
+codebook("## Data output files")
+codebook("Name                | Description")
+codebook("--------------------|------------")
+codebook("`DataSet.txt`       | Overall data set with subject, activity, and all experimental measurements") 
+codebook("`TidyData.txt`      | Tidy data set with average of each variable for each activity and each subject")
+codebook("")
+
+
+############################### 
 ## All Project Goals complete##
 ###############################
 
